558cc08c018ce9ebe6334000bd8a4d5e3577c71d
doodzik <4004blog@gmail.com>
2014-11-01 18:37:47 +0100

    add caller information

--- lib/CallBaecker.rb
+++ lib/CallBaecker.rb
@@ -22,13 +22,14 @@ module CallBaecker
       @__last_methods_added = [name, with, without]
       _self = self
       define_method with do |*args, &block|
+        callee = caller.first
         @__last_args = *args
         catch :CallBaeckerDone do
           # before hook
           # TODO make it pluggable
           # it shouldnt be limited to rubisierung
           if has_before_hook
-            *args = _self.instance_variable_get(:@__before_hook).call(params, args[0])
+            *args = _self.instance_variable_get(:@__before_hook).call(params, args[0], callee)
           end
           send without, *args, &block
         end
@@ -56,12 +57,13 @@ module CallBaecker
       @__last_methods_added = [name, with, without]
       define_singleton_method with do |*args, &block|
         @__last_args = *args
+        callee = caller.first
         catch :CallBaeckerDone do
           # before hook
           # TODO make it pluggable
           # it shouldnt be limited to rubisierung
           if has_before_hook  
-            *args = @__before_hook.call(params, args[0])
+            *args = @__before_hook.call(params, args[0], callee)
           end
           send without, *args, &block
         end
